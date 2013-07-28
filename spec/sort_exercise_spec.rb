cbc454d83d1db26be6a5e901dbc72aeb9ea16c9f
Tamara Temple <tamouse@gmail.com>
2013-07-27 23:16:49 -0500

    extend test to array of strings

--- spec/sort_exercise_spec.rb
+++ spec/sort_exercise_spec.rb
@@ -3,20 +3,34 @@ require 'spec_helper'
 module SortExercise
   
   describe SortExercise do
-    let(:a) {(1..7).to_a.shuffle}
 
     it 'should have a version number' do
       SortExercise::VERSION.should_not be_nil
     end
 
-    it 'should sort by looping' do
-      SortExercise::verify_sort(SortExercise::loop_sort(a)).should be_true
+    context "numeric array" do
+      let(:a) {(1..7).to_a.shuffle}
+
+      it 'should sort by looping' do
+        SortExercise::verify_sort(SortExercise::loop_sort(a)).should be_true
+      end
+      
+      it 'should sort by recursing' do
+        SortExercise::verify_sort(SortExercise::recursive_sort(a)).should be_true
+      end
     end
 
-    it 'should sort by recursing' do
-      SortExercise::verify_sort(SortExercise::recursive_sort(a)).should be_true
+    context "string array" do
+      let(:b) {%w{now is the winter of our discount tent}.shuffle}
+
+      it 'should sort by looping' do
+        SortExercise::verify_sort(SortExercise::loop_sort(b)).should be_true
+      end
+      
+      it 'should sort by recursing' do
+        SortExercise::verify_sort(SortExercise::recursive_sort(b)).should be_true
+      end
     end
-    
   end
 
 end
