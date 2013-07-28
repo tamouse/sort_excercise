b5566e9036035e2209fbad3dad0cebd799775ca4
Tamara Temple <tamouse@gmail.com>
2013-07-27 23:10:56 -0500

    all sorts working, exercise done

--- lib/sort_exercise.rb
+++ lib/sort_exercise.rb
@@ -2,14 +2,47 @@ require "sort_exercise/version"
 
 module SortExercise
 
-  module_function
-
-  def loop_sort(a)
-    a.sort
+  # Implement a simple insertion sort in a loop
+  def self.loop_sort(a)
+    a.each_with_index do |element, index|
+      hole = (index - 1)
+      # Walk backwards through the array until we find the hole
+      while hole >= 0
+        break if a[hole] <= element
+        a[hole + 1] = a[hole]
+        hole -= 1
+      end
+      a[hole+1] = element
+    end
   end
 
-  def recursive_sort(a)
-    a.sort
+  def self.recursive_sort(a,first=nil,last=nil)
+    first = 0 if first.nil?
+    last = a.size if last.nil?
+    if (first < last)
+      recursive_sort(a, first, last-1)
+      self.insert_in_order(a.last, a, first, last-1)
+    end
+    a
+  end
+
+  def self.verify_sort(a)
+    a == a.sort
+  end
+
+  private
+
+  def self.insert_in_order(el, a, first, last)
+    if (el >= a[last])
+      a[last+1] = el
+    elsif (first < last)
+      a[last+1] = a[last]
+      insert_in_order(el, a, first, last-1)
+    else
+      a[last+1] = a[last]
+      a[last] = el
+    end
+    a
   end
 
 end
--- spec/sort_exercise_spec.rb
+++ spec/sort_exercise_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 module SortExercise
   
   describe SortExercise do
+    let(:a) {(1..7).to_a.shuffle}
+
     it 'should have a version number' do
       SortExercise::VERSION.should_not be_nil
     end
 
     it 'should sort by looping' do
-      a = (1..7).to_a.shuffle
-      SortExercise::loop_sort(a).should == a.sort
+      SortExercise::verify_sort(SortExercise::loop_sort(a)).should be_true
     end
 
     it 'should sort by recursing' do
-      a = (1..7).to_a.shuffle
-      SortExercise::recursive_sort(a).should == a.sort
+      SortExercise::verify_sort(SortExercise::recursive_sort(a)).should be_true
     end
     
   end
